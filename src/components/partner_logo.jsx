c6f8678cf7fe94efdc75bdbbb5cdd91b73290be6
faysalzaman <faysalzaman00@gmail.com>
2024-06-27 01:22:15 +0500

    Updated

--- src/components/partner_logo.jsx
+++ src/components/partner_logo.jsx
@@ -1,14 +1,22 @@
 // Import your logo images
-import hpLogo from "https://res.cloudinary.com/dqph3qip7/image/upload/v1719431537/ts_geeks/hp.png";
-import lenovoLogo from "https://res.cloudinary.com/dqph3qip7/image/upload/v1719431537/ts_geeks/lenovo.png";
-import vmwareLogo from "https://res.cloudinary.com/dqph3qip7/image/upload/v1719431537/ts_geeks/vmware.png";
-import dellLogo from "https://res.cloudinary.com/dqph3qip7/image/upload/v1719431537/ts_geeks/dell.png";
-import microsoftLogo from "https://res.cloudinary.com/dqph3qip7/image/upload/v1719431537/ts_geeks/mircosoft.png";
-import mitelLogo from "https://res.cloudinary.com/dqph3qip7/image/upload/v1719431537/ts_geeks/mitel.png";
-import intelLogo from "https://res.cloudinary.com/dqph3qip7/image/upload/v1719431537/ts_geeks/intel.png";
-import mikrotikLogo from "https://res.cloudinary.com/dqph3qip7/image/upload/v1719431537/ts_geeks/mikrotik.png";
 
 const PartnerLogos = () => {
+  let hpLogo =
+    "https://res.cloudinary.com/dqph3qip7/image/upload/v1719431537/ts_geeks/hp.png";
+  let lenovoLogo =
+    "https://res.cloudinary.com/dqph3qip7/image/upload/v1719431537/ts_geeks/lenovo.png";
+  let vmwareLogo =
+    "https://res.cloudinary.com/dqph3qip7/image/upload/v1719431537/ts_geeks/vmware.png";
+  let dellLogo =
+    "https://res.cloudinary.com/dqph3qip7/image/upload/v1719431537/ts_geeks/dell.png";
+  let microsoftLogo =
+    "https://res.cloudinary.com/dqph3qip7/image/upload/v1719431537/ts_geeks/mircosoft.png";
+  let mitelLogo =
+    "https://res.cloudinary.com/dqph3qip7/image/upload/v1719431537/ts_geeks/mitel.png";
+  let intelLogo =
+    "https://res.cloudinary.com/dqph3qip7/image/upload/v1719431537/ts_geeks/intel.png";
+  let mikrotikLogo =
+    "https://res.cloudinary.com/dqph3qip7/image/upload/v1719431537/ts_geeks/mikrotik.png";
   return (
     <div
       className="container mx-auto py-16 bg-white flex items-center justify-center pl-10 pr-10"
