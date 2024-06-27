86db43657db0c332e1eda4a23226866d7e5deaf5
faysalzaman <faysalzaman00@gmail.com>
2024-06-27 12:07:52 +0500

    style: Update header and AllRightReserved components
    Refactor the header and AllRightReserved components to improve the layout and styling.
    
    - In the AllRightReserved component, add a white background and adjust the logo and text alignment.
    - In the header component, remove the fixed positioning and adjust the logo size and margin.
    
    This commit addresses the visual improvements needed for the header and AllRightReserved components.

--- src/components/all_right_reserved.jsx
+++ src/components/all_right_reserved.jsx
@@ -1,12 +1,14 @@
 function AllRightReserved() {
   return (
-    <div className="all-right-reserved mt-10 mb-5 flex flex-row items-center justify-center">
+    <div className="all-right-reserved mt-10 mb-5 flex flex-col md:flex-row items-center justify-center bg-white">
       <img
         src="https://res.cloudinary.com/dqph3qip7/image/upload/v1719431556/ts_geeks/logo.png"
         alt="Logo"
-        className="logo w-52 mr-10"
+        className="logo w-52 mb-5 md:mb-0 md:mr-10"
       />
-      <p>© Copyright 2024 T&S Tech Geeks - All Rights Reserved</p>
+      <p className="items-center justify-center text-center">
+        © Copyright 2024 T&S Tech Geeks - All Rights Reserved
+      </p>
     </div>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,11 @@
 const Header = () => {
   return (
-    <header className="shadow fixed z-50 w-full bg-white">
-      <div className="container mx-auto px-4 py-4 flex items-center justify-between">
+    <header className="shadow z-50 w-full bg-white">
+      <div className="container mx-auto px-4 py-4 flex flex-col md:flex-row items-center justify-between">
         <img
           src="https://res.cloudinary.com/dqph3qip7/image/upload/v1719431556/ts_geeks/logo.png"
           alt="Logo"
-          className="h-16 w-fit mr-2"
+          className="h-16 md:h-12 lg:h-20 w-auto mr-0 md:mr-2 mb-4 md:mb-0"
         />
       </div>
     </header>
