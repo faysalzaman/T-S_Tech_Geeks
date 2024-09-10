c92e5750b94894860896ed23f30270a05838a0ab
faysalzaman <faysalzaman00@gmail.com>
2024-09-10 12:30:00 +0500

    style: Adjust logo size in all_right_reserved and header components

--- src/components/all_right_reserved.jsx
+++ src/components/all_right_reserved.jsx
@@ -4,7 +4,7 @@ function AllRightReserved() {
       <img
         src="https://res.cloudinary.com/dqph3qip7/image/upload/v1725720381/footer_n3hvr8.png"
         alt="Logo"
-        className="logo w-52 mb-5 md:mb-0 md:mr-10"
+        className="bg-transparent logo w-40 md:w-48 lg:w-52 mb-5 md:mb-0 md:mr-10" // Adjusted logo size
       />
       <p className="items-center justify-center text-center">
         © Copyright 2024 TechNova IT - All Rights Reserved
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,11 @@
 const Header = () => {
   return (
     <header className="shadow z-50 w-full bg-black">
-      <div className="container mx-auto px-4 py-4 flex flex-col md:flex-row items-center justify-between">
+      <div className="container mx-auto px-4 py-2 md:py-3 flex items-center justify-between object-cover">
         <img
           src="https://res.cloudinary.com/dqph3qip7/image/upload/v1725720858/header_qf9pe9.png"
           alt="Logo"
-          className="h-24 md:h-20 lg:h-28 w-auto mr-0 md:mr-2 mb-4 md:mb-0"
+          className="h-16 md:h-20 lg:h-24 w-auto object-cover"
         />
       </div>
     </header>
